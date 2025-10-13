eadd8426792956ae274052f8cb92731ca529aa37
LMSLima <luizmiguel.lima1402@email.com>
2025-10-13 05:04:04 -0300

    Atualização

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
+const BASE = import.meta.env.VITE_API_BASE_URL || "https://backend-t20web.onrender.com";
 
 const api = axios.create({
   baseURL: BASE,
