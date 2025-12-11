09e3ff9747754d240f75477d31aa0f43670b1984
LMSLima <luizmiguel.lima1402@email.com>
2025-12-11 00:17:29 -0300

    Pronto

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 // Base da API
-const BASE = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
+const BASE = import.meta.env.VITE_API_BASE_URL || "https://backend-t20web-lnil.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE,
